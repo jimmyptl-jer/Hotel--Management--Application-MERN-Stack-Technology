0883e4315e9c239fff3260fd34fee6634584f087
Jimmy Patel <jimmypatel678@yahoo.com>
2024-01-24 20:04:11 -0500

    Removed Booking currently

--- frontend/src/Pages/Booking.tsx
+++ frontend/src/Pages/Booking.tsx
@@ -1,97 +1,97 @@
-import { useSearchContext } from "../Context/SearchContext";
-import { useParams } from "react-router-dom";
-import { useQuery } from "react-query";
-import * as apiClient from "../api-client";
-import { useEffect, useState } from "react";
-import BookingForm from "../forms/BookingForm/BookingForm";
-import BookingDetailsSummary from "../Components/BookingDetailsSummary";
-import { useAppContext } from "../Context/AppContext";
-import { Elements } from "@stripe/react-stripe-js";
+// import { useSearchContext } from "../Context/SearchContext";
+// import { useParams } from "react-router-dom";
+// import { useQuery } from "react-query";
+// import * as apiClient from "../api-client";
+// import { useEffect, useState } from "react";
+// import BookingForm from "../forms/BookingForm/BookingForm";
+// import BookingDetailsSummary from "../Components/BookingDetailsSummary";
+// import { useAppContext } from "../Context/AppContext";
+// import { Elements } from "@stripe/react-stripe-js";
 
-// Main Booking Component
-const Booking = () => {
-  // State for the number of nights
-  const [numberOfNights, setNumberOfNights] = useState(0);
+// // Main Booking Component
+// const Booking = () => {
+//   // State for the number of nights
+//   const [numberOfNights, setNumberOfNights] = useState(0);
 
-  // Accessing search context and extracting hotelId from URL parameters
-  const search = useSearchContext();
-  const { hotelId } = useParams();
+//   // Accessing search context and extracting hotelId from URL parameters
+//   const search = useSearchContext();
+//   const { hotelId } = useParams();
 
-  // Query to fetch hotel details by hotelId
-  const { data: hotel } = useQuery(
-    "fetchHotelById",
-    () => apiClient.fetchHotelById(hotelId as string),
-    {
-      enabled: !!hotelId,
-    },
-  );
+//   // Query to fetch hotel details by hotelId
+//   const { data: hotel } = useQuery(
+//     "fetchHotelById",
+//     () => apiClient.fetchHotelById(hotelId as string),
+//     {
+//       enabled: !!hotelId,
+//     },
+//   );
 
-  // Query to create a payment intent
-  const { data: paymentIntentData } = useQuery(
-    "createPaymentIntent",
-    () =>
-      apiClient.createPaymentIntent(
-        hotelId as string,
-        numberOfNights.toString(),
-      ),
-    {
-      enabled: !!hotelId && numberOfNights > 0,
-    },
-  );
+//   // Query to create a payment intent
+//   const { data: paymentIntentData } = useQuery(
+//     "createPaymentIntent",
+//     () =>
+//       apiClient.createPaymentIntent(
+//         hotelId as string,
+//         numberOfNights.toString(),
+//       ),
+//     {
+//       enabled: !!hotelId && numberOfNights > 0,
+//     },
+//   );
 
-  // Accessing stripePromise from AppContext
-  const { stripePromise } = useAppContext();
+//   // Accessing stripePromise from AppContext
+//   const { stripePromise } = useAppContext();
 
-  // Calculate the number of nights based on check-in and check-out dates
-  useEffect(() => {
-    if (search?.checkIn && search?.checkOut) {
-      const night =
-        Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) /
-        (1000 * 60 * 60 * 24);
-      setNumberOfNights(Math.ceil(night));
-    }
-  }, [search.checkIn, search.checkOut]);
+//   // Calculate the number of nights based on check-in and check-out dates
+//   useEffect(() => {
+//     if (search?.checkIn && search?.checkOut) {
+//       const night =
+//         Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) /
+//         (1000 * 60 * 60 * 24);
+//       setNumberOfNights(Math.ceil(night));
+//     }
+//   }, [search.checkIn, search.checkOut]);
 
-  // Query to fetch current user details
-  const { data: currentUser } = useQuery(
-    "fetchCurrentUser",
-    apiClient.fetchCurrentUser,
-  );
+//   // Query to fetch current user details
+//   const { data: currentUser } = useQuery(
+//     "fetchCurrentUser",
+//     apiClient.fetchCurrentUser,
+//   );
 
-  // If hotel data is not available, return an empty fragment
-  if (!hotel) {
-    return <></>;
-  }
+//   // If hotel data is not available, return an empty fragment
+//   if (!hotel) {
+//     return <></>;
+//   }
 
-  // Render the BookingDetailsSummary and BookingForm components
-  return (
-    <div className="grid md:grid-cols-[1fr_2fr]">
-      {/* Display booking details summary */}
-      <BookingDetailsSummary
-        checkIn={search.checkIn}
-        checkOut={search.checkOut}
-        adultCount={search.adultCount}
-        childCount={search.childCount}
-        numberOfNights={numberOfNights}
-        hotel={hotel}
-      />
-      {/* Display booking form if there is a logged-in user and payment intent data */}
-      {currentUser && paymentIntentData && (
-        <Elements
-          stripe={stripePromise}
-          options={{
-            clientSecret: paymentIntentData.clientSecret,
-          }}
-        >
-          <BookingForm
-            currentUser={currentUser}
-            paymetIntent={paymentIntentData}
-          />
-        </Elements>
-      )}
-    </div>
-  );
-};
+//   // Render the BookingDetailsSummary and BookingForm components
+//   return (
+//     <div className="grid md:grid-cols-[1fr_2fr]">
+//       {/* Display booking details summary */}
+//       <BookingDetailsSummary
+//         checkIn={search.checkIn}
+//         checkOut={search.checkOut}
+//         adultCount={search.adultCount}
+//         childCount={search.childCount}
+//         numberOfNights={numberOfNights}
+//         hotel={hotel}
+//       />
+//       {/* Display booking form if there is a logged-in user and payment intent data */}
+//       {currentUser && paymentIntentData && (
+//         <Elements
+//           stripe={stripePromise}
+//           options={{
+//             clientSecret: paymentIntentData.clientSecret,
+//           }}
+//         >
+//           <BookingForm
+//             currentUser={currentUser}
+//             paymetIntent={paymentIntentData}
+//           />
+//         </Elements>
+//       )}
+//     </div>
+//   );
+// };
 
-// Export the Booking component as the default export
-export default Booking;
+// // Export the Booking component as the default export
+// export default Booking;
--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -1,74 +1,75 @@
-import {
-  UserType,
-  paymentIntentResponse,
-} from "../../../../backend/src/shared/types";
-import { useForm } from "react-hook-form";
+// import {
+//   UserType,
+//   paymentIntentResponse,
+// } from "../../../../backend/src/shared/types";
+// import { useForm } from "react-hook-form";
 
-type Props = {
-  currentUser: UserType;
-  paymentIntent: paymentIntentResponse;
-};
+// type Props = {
+//   currentUser: UserType;
+//   paymentIntent: paymentIntentResponse;
+// };
 
-type BookingFormData = {
-  firstName: string;
-  lastName: string;
-  email: string;
-};
+// type BookingFormData = {
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+// };
 
-const BookingForm = ({ currentUser, paymentIntent }: Props) => {
-  const { register, handleSubmit } = useForm<BookingFormData>({
-    defaultValues: {
-      firstName: currentUser.firstName,
-      lastName: currentUser.lastName,
-      email: currentUser.email,
-    },
-  });
+// const BookingForm = ({ currentUser, paymentIntent }: Props) => {
+//   const { register, handleSubmit } = useForm<BookingFormData>({
+//     defaultValues: {
+//       firstName: currentUser.firstName,
+//       lastName: currentUser.lastName,
+//       email: currentUser.email,
+//     },
+//   });
 
-  return (
-    <form className="grid grid-cols-1 gap-5 rounded-lg border border-slate-300 p-3">
-      <span className="text-3xl font-bold">Confirm Your Details</span>
-      <div className="grid grid-cols-2 gap-6">
-        <label className="text-gray-700 text-sm font-bold flex-1">
-          First Name
-          <input
-            className="border mt-1 rounded w-full py-2 px-3 text-gray-800 bg-gray-400"
-            type="text"
-            readOnly
-            disabled
-            {...register("firstName")}
-          />
-        </label>
+//   return (
+//     <form className="grid grid-cols-1 gap-5 rounded-lg border border-slate-300 p-3">
+//       <span className="text-3xl font-bold">Confirm Your Details</span>
+//       <div className="grid grid-cols-2 gap-6">
+//         <label className="text-gray-700 text-sm font-bold flex-1">
+//           First Name
+//           <input
+//             className="border mt-1 rounded w-full py-2 px-3 text-gray-800 bg-gray-400"
+//             type="text"
+//             readOnly
+//             disabled
+//             {...register("firstName")}
+//           />
+//         </label>
 
-        <label className="text-gray-700 text-sm font-bold flex-1">
-          Last Name
-          <input
-            className="border mt-1 rounded w-full py-2 px-3 text-gray-800 bg-gray-400"
-            type="text"
-            readOnly
-            disabled
-            {...register("lastName")}
-          />
-        </label>
+//         <label className="text-gray-700 text-sm font-bold flex-1">
+//           Last Name
+//           <input
+//             className="border mt-1 rounded w-full py-2 px-3 text-gray-800 bg-gray-400"
+//             type="text"
+//             readOnly
+//             disabled
+//             {...register("lastName")}
+//           />
+//         </label>
 
-        <label className="text-gray-700 text-sm font-bold">
-          Email
-          <input
-            className="border mt-1 rounded w-full py-2 px-3 text-gray-800 bg-gray-400"
-            type="email"
-            readOnly
-            disabled
-            {...register("email")}
-          />
-        </label>
-      </div>
+//         <label className="text-gray-700 text-sm font-bold">
+//           Email
+//           <input
+//             className="border mt-1 rounded w-full py-2 px-3 text-gray-800 bg-gray-400"
+//             type="email"
+//             readOnly
+//             disabled
+//             {...register("email")}
+//           />
+//         </label>
+//       </div>
 
-      <div className="bg-blue-200 p-4 rounded-md">
-        <div className="font-semi-bold text-lg">
-          Total Cost : ${paymentIntent.totalCost.toFixed(2)}
-        </div>
-      </div>
-    </form>
-  );
-};
+//       <div className="bg-blue-200 p-4 rounded-md">
+//         <div className="font-semi-bold text-lg">
+//           Total Cost : ${paymentIntent.totalCost.toFixed(2)}
+//         </div>
+//         <div className="text-xs"></div>
+//       </div>
+//     </form>
+//   );
+// };
 
-export default BookingForm;
+// export default BookingForm;
